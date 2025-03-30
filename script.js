707a8fec6ddcc236fb18bc4228e2d21c7a46af1c
gopiraju9000 <144523885+gopiraju9000@users.noreply.github.com>
2025-03-31 00:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function mapLetters(str) {
+
+	let obj = new Object();
+
+	for(let i = 0;i < str.length; i++){
+
+		let ch = str.charAt(i);
+
+		if(obj[ch] == null) {
+			obj[ch] = [i];
+		}
+
+		else {
+			obj[ch].push(i);
+		}
+	}
+
+	return obj;
+}
+
+const str = prompt("Enter the str:  ")
+alert(JSON.stringify(mapLetters(str)))
